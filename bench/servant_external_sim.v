bfc55ba5b415b4a42f49cba40f48ca08d9d800bb
Alfred Persson Forsberg <cat@catcream.org>
2025-05-06 14:28:28 +0200

    sw/interrupt_interrupt -> sw/external_interrupt

--- bench/servant_external_sim.v
+++ bench/servant_external_sim.v
@@ -1,12 +1,14 @@
 `default_nettype none
-module servant_external_sim
+module servant_interrupt_sim
   (input wire	      wb_clk,
-   input wire	      timer_clk,
-   input wire	      wb_rst,
+   input wire         wb_rst,
+   input wire         ext_irq,
    output wire [31:0] pc_adr,
-   output wire	      pc_vld,
-   output wire	      q,
-   input wire	      ext_irq
+   output wire        pc_vld,
+   output wire        q,
+   output wire [3:0]  mcause3_0,
+   output wire        new_irq,
+   output wire        mret
 );
 
    parameter memfile = "";
@@ -20,10 +22,10 @@ module servant_external_sim
    initial
      if ($value$plusargs("firmware=%s", firmware_file)) begin
 	$display("Loading RAM from %0s", firmware_file);
-	$readmemh(firmware_file, dut.ram.mem);
+	$readmemh(firmware_file, dut.servant.ram.mem);
      end
 
-   servant
+   servant_sleep_dummy
      #(.memfile  (memfile),
        .memsize  (memsize),
        .width    (width),
@@ -32,10 +34,11 @@ module servant_external_sim
        .with_csr (with_csr),
        .compress (compressed[0:0]),
        .align    (align[0:0]))
-   dut(wb_clk, timer_clk, wb_rst, q);
+   dut(wb_clk, wb_rst, ext_irq, q);
 
-   assign pc_adr = dut.wb_mem_adr;
-   assign pc_vld = dut.wb_mem_ack;
-   assign dut.external_irq = ext_irq;
+   assign pc_adr = dut.servant.wb_mem_adr;
+   assign pc_vld = dut.servant.wb_mem_ack;
+   assign new_irq = dut.servant.cpu.cpu.new_irq;
+   assign mret = dut.servant.cpu.cpu.mret;
 
 endmodule
--- bench/servant_external_tb.cpp
+++ bench/servant_external_tb.cpp
@@ -3,13 +3,15 @@
 #include <signal.h>
 
 #include "verilated_vcd_c.h"
-#include "Vservant_external_sim.h"
+#include "Vservant_interrupt_sim.h"
 
 #include <ctime>
 
 using namespace std;
 
 static bool done;
+
+#define TIMER_CLK_DIV 3
 
 vluint64_t main_time = 0;       // Current simulation time
 // This is a 64-bit integer to reduce wrap over issues and
@@ -20,19 +22,19 @@ double sc_time_stamp () {       // Called by $time in Verilog
   // what SystemC does
 }
 
-void INThandler(int signal)
-{
+void INThandler(int signal) {
   printf("\nCaught ctrl-c\n");
   done = true;
 }
 
-int main(int argc, char **argv, char **env)
-{
-  int baud_rate = 0;
+int reset(Vservant_interrupt_sim *, VerilatedVcdC *);
+int timer_test(Vservant_interrupt_sim *, VerilatedVcdC *, vluint64_t);
+int external_test(Vservant_interrupt_sim *, VerilatedVcdC *, vluint64_t);
 
+int main(int argc, char **argv, char **env) {
   Verilated::commandArgs(argc, argv);
 
-  Vservant_external_sim* top = new Vservant_external_sim;
+  Vservant_interrupt_sim* top = new Vservant_interrupt_sim;
 
   VerilatedVcdC * tfp = 0;
   const char *vcd = Verilated::commandArgsPlusMatch("vcd=");
@@ -62,34 +64,55 @@ int main(int argc, char **argv, char **env)
   top->wb_clk = 1;
   bool q = top->q;
   int clock = 0;
-  int cnt = 0;
-  top->ext_irq = 0;
-  while (!(done || Verilated::gotFinish())) {
+  timer_test(top, tfp, timeout);
+  reset(top, tfp);
+  timer_test(top, tfp, timeout);
+  if (tfp) {
+    tfp->close();
+  }
+  exit(0);
+}
+
+
+int reset(Vservant_interrupt_sim *top, VerilatedVcd *tfp) {
+    top->wb_rst = 1;
+    for (int i = 0; i < 10; i++) {
+        top->wb_clk = !top->wb_clk;
+        top->eval();
+        if (tfp) {
+            tfp->dump(main_time);
+        }
+    }
+    top->wb_rst = 0;
+    return 0;
+}
+
+int external_test(Vservant_interrupt_sim *top, VerilatedVcdC *tfp, vluint64_t timeout) {
+  int clock = 0;
+}
+
+int timer_test(Vservant_interrupt_sim *top, VerilatedVcdC *tfp, vluint64_t timeout) {
+  int clock = 0;
+  while (Verilated::gotFinish()) {
     clock++;
-    top->wb_rst = main_time < 100;
+    top->wb_rst = main_time < 1000;
     top->eval();
     if (tfp) {
       tfp->dump(main_time);
     }
     if (top->wb_clk && top->pc_vld && top->pc_adr) {
       std::this_thread::sleep_for(std::chrono::milliseconds(500));
-      printf("%d\n", top->pc_adr);
-      if (top->pc_adr == 32) {
-	if (cnt > 5) {
-	  top->ext_irq = 1;
-	}
-	cnt++;	
+      printf("%d | %d\n", clock, top->pc_adr);
+      if (top->mret) {
+        printf("mret detected, exiting\n");
+        exit(0);
       }
     }
     if (timeout && (main_time >= timeout)) {
       printf("Timeout: Exiting at time %lu\n", main_time);
-      done = true;
+      exit(0);
     }
     top->wb_clk = !top->wb_clk;
     main_time+=31.25;
   }
-  if (tfp) {
-    tfp->close();
-  }
-  exit(0);
 }
